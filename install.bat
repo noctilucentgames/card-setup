ac54ab1165cadc6d820b3531ae80844624cf5c41
noctilucentgames <noctilucentgames@gmail.com>
2021-04-29 22:28:41 +0200

    patch 105

--- install.bat
+++ install.bat
@@ -3,17 +3,17 @@ if not exist %~dp0\..\..\data\opcodes (echo Invalid location of installation fil
 if not exist %~dp0\..\..\data\definitions (echo Invalid location of installation file, expected to be in TeraToolbox\mods\card-setup&&pause&&exit)
 if not exist %~dp0\install (echo Missing install folder %~dp0install&&pause&&exit)
 (echo. && ^
-echo C_REMOVE_CARD_COLLECTION_EFFECT 58294 && ^
-echo S_REMOVE_CARD_COLLECTION_EFFECT 48550 && ^
-echo C_ADD_CARD_COLLECTION_EFFECT 35029 && ^
-echo S_ADD_CARD_COLLECTION_EFFECT 28226 && ^
-echo C_CARD_PRESET 34365 && ^
-echo S_CARD_PRESET 29768 && ^
-echo S_CARD_COLLECTION_EFFECTS 44837 && ^
-echo S_ADD_CARD_TO_PRESET 53019 && ^
-echo S_REMOVE_CARD_FROM_PRESET 25441 && ^
-echo S_CARD_DATA 36253 && ^
-echo.) >> %~dp0\..\..\data\opcodes\protocol.380186.map
+echo C_REMOVE_CARD_COLLECTION_EFFECT 20541 && ^
+echo S_REMOVE_CARD_COLLECTION_EFFECT 65443 && ^
+echo C_ADD_CARD_COLLECTION_EFFECT 30699 && ^
+echo S_ADD_CARD_COLLECTION_EFFECT 39384 && ^
+echo C_CARD_PRESET 36656 && ^
+echo S_CARD_PRESET 21610 && ^
+echo S_CARD_COLLECTION_EFFECTS 45492 && ^
+echo S_ADD_CARD_TO_PRESET 49581 && ^
+echo S_REMOVE_CARD_FROM_PRESET 53384 && ^
+echo S_CARD_DATA 22061 && ^
+echo.) >> %~dp0\..\..\data\opcodes\protocol.381290.map
 xcopy /s/r/y %~dp0\install %~dp0\..\..\data\definitions >NUL
 echo Installation successfull.
 pause
